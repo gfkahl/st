5a3810fdb28ad3e91e378c52b486670dcc8b5d91
gfkahl <gfkahl@gmail.com>
2021-06-14 19:29:07 -0400

    Added alpha and alpha focus patches

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.97, alphaUnfocused = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -120,6 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"black",
 };
 
 
@@ -131,6 +135,7 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
+unsigned int bg = 256, bgUnfocused = 256;
 
 /*
  * Default shape of cursor
